347e98561931ee3681625f063d978629c7c1d31d
gcachuo <gcachu.o@gmail.com>
2015-06-13 18:12:01 -0500

    agregar

---
+++ CashPau/misServicios.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title></title>
+    <link href="css/site.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="col-md-10">
+    </div>
+    <div class="col-md-1">
+        <form action="home.php" method="post">
+            <input value="back" name="back" type="hidden" />
+            <input type="submit" class="col-md-12 btn-primary" value="Regresar" />
+        </form>
+    </div>
+    <div class="col-md-12">
+        <div class="col-md-12">
+           
+            <h3 class="col-md-6">NUEVO SERVICIO</h3>
+               <h3 class="align-right col-md-5">SERVICIOS ACTUALES</h3>
+        </div>
+        <div class="col-md-1">
+            
+        </div>
+        <div class="col-md-3">
+            <div class="col-md-6">
+            <input type="button" class="col-md-12 btn-primary" value="Guardar" />
+                </div>
+            <div class="col-md-6">
+            <input type="button" class="col-md-12 btn-primary" value="Cancelar" />
+                </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ CashPau/registrarUser.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title></title>
+</head>
+<body>
+    <div class="col-md-10">
+    </div>
+    <div class="col-md-1">
+        <form action="home.php" method="post">
+            <input value="back" name="back" type="hidden" />
+            <input type="submit" class="col-md-12 btn-primary" value="Regresar" />
+        </form>
+
+
+    </div>
+    <div class="col-md-12">
+        <div class="col-md-2">
+        </div>
+        <div class="col-md-2">
+           
+             <div>
+            <img src="" alt="imagen" width="150" height="150" />
+                  </div>
+                  <div>
+            <input type="button" class="col-md-9 btn-primary" value="Subir Foto" />
+                </div>
+               
+        </div>
+        <div class="col-md-4">
+            <div class="col-md-12">
+                <label class="col-md-6">Nombre:</label>
+                <input type="text" class="col-md-6 " placeholder="Nombre" />
+            </div>
+            <div class="col-md-12">
+                <label class="col-md-6">Apellido Paterno:</label>
+                <input type="text" class="col-md-6 " placeholder="Apellido Paterno" />
+            </div>
+            <div class="col-md-12">
+                <label class="col-md-6">Apellido Materno:</label>
+                <input type="text" class="col-md-6 " placeholder="Apellido Materno" />
+            </div>
+             <div class="col-md-12">
+                <label class="col-md-6">Usuario:</label>
+                <input type="text" class="col-md-6 " placeholder="Usuario" />
+            </div>
+             <div class="col-md-12">
+                <label class="col-md-6">Password:</label>
+                <input type="password" class="col-md-6 " placeholder="Password" />
+            </div>
+             <div class="col-md-12">
+                <label class="col-md-6">Re-Password:</label>
+                <input type="password" class="col-md-6 " placeholder="Re-Password" />
+            </div>
+             <div class="col-md-12">
+                <label class="col-md-6">Fecha de Nacimiento:</label>
+                <input type="date" class="col-md-6 " />
+            </div>
+            <div class="col-md-12">
+                <label class="col-md-6">Correo:</label>
+                <input type="text" class="col-md-6 " placeholder="Correo" />
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="col-md-12">
+            <input type="button" class="col-md-12 btn-primary" value="Registrarse" />
+                </div>
+            <div class="col-md-12">
+            <input type="button" class="col-md-12 btn-primary" value="Limpiar" />
+                </div>
+        </div>
+    </div>
+</body>
+</html>
